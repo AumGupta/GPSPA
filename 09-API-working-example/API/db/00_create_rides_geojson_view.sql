f5f451eebb1e0e7069606166fc786a170eeab69f
Om Gupta <aaumgupta@gmail.com>
2026-02-07 20:47:01 +0000

    Refactor API example notebook and add .gitignore
    - Updated execution counts in the Jupyter notebook to reflect the correct order.
    - Added print statement to display the type of the response object.
    - Enhanced output formatting for the response data in the notebook.
    - Added a .gitignore file to exclude the project directory from version control.
    - Added compiled Python bytecode for utility functions in the API.

--- 09-API-working-example/API/db/00_create_rides_geojson_view.sql
+++ 09-API-working-example/API/db/00_create_rides_geojson_view.sql
@@ -3,5 +3,9 @@ SELECT id,
 	pickup_datetime, 
 	dropoff_datetime,
 	passenger_count,
+	rate_code,
+	payment_type,
+	tip_amount,
+	total_amount,
 	st_asGeoJSON(st_makeline(pickup, dropoff))::json as geojson
 from pa.rides; 
--- 09-API-working-example/API/main.py
+++ 09-API-working-example/API/main.py
@@ -1,17 +1,18 @@
+import os
 from flask import (
     Flask, 
     request,
     jsonify,
-    sql,
     )
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from psycopg2.pool import SimpleConnectionPool
 from utils import format_geojson
 
 DB_CONFIG = {
     "database": "taxidb",
-    "user": "aneto",
-    "password": "castor107",
+    "user": "postgres",
+    "password": "ogroot",
     "host": "localhost",
     "port": "5432"}
 
@@ -20,32 +21,48 @@ DB_CONFIG = {
 # import os
 # DB_CONFIG = {}
 # DB_CONFIG['database'] = os.environ.get('DATABASE')
-# DB_CONFIG['username'] = os.environ.get('USERNAME')
+# DB_CONFIG['password'] = os.environ.get('POSTGRES_PASSWORD')
+# print(DB_CONFIG)
 # ...
+
+# Create pooling to the database maximum 10 connections
+# This allows to economize database connections
+db_pool = SimpleConnectionPool(
+    minconn=1,
+    maxconn= 10,
+    database=DB_CONFIG["database"],
+    user=DB_CONFIG["user"],
+    password=DB_CONFIG["password"],
+    host=DB_CONFIG["host"],
+    port=DB_CONFIG["port"],
+    cursor_factory=RealDictCursor
+)
 
 # Create a flask application
 app = Flask(__name__)
 
 # Database connection function
+# that makes use of pooling
 def get_db_connection():
-    return psycopg2.connect(
-        database=DB_CONFIG["database"],
-        user=DB_CONFIG["user"],
-        password=DB_CONFIG["password"],
-        host=DB_CONFIG["host"],
-        port=DB_CONFIG["port"],
-        cursor_factory=RealDictCursor
-    )
+    return db_pool.getconn()
 
-# GET all rides from pa.rides_geojson
+# Function to release the connection and
+# make it available in the pooling
+def release_db_connection(conn):
+    db_pool.putconn(conn)
+
+# GET "all" rides from pa.rides
+# We are limiting it to 50 to be faster to retrieve
 @app.route('/rides3', methods=['GET'])
 def get_rides():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("""SELECT * FROM pa.rides LIMIT 50""")
-    rides = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    try:
+        cursor.execute("""SELECT * FROM pa.rides LIMIT 50""")
+        rides = cursor.fetchall()
+    finally:
+        cursor.close()
+        release_db_connection(conn)
     return jsonify(rides)
 
 # GET all rides from pa.rides_geojson getting a GeoJSON column
@@ -53,15 +70,20 @@ def get_rides():
 def get_rides2():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("""SELECT id,
-	                    pickup_datetime, 
-	                    dropoff_datetime,
-	                    passenger_count,
-	                    st_asGeoJSON(st_makeline(pickup, dropoff))::json as geom
-                      FROM pa.rides Limit 50;""")
-    rides = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    try:
+        cursor.execute(
+            """SELECT id,
+	           pickup_datetime, 
+	           dropoff_datetime,
+	           passenger_count,
+	           st_asGeoJSON(st_makeline(pickup, dropoff))::json as geom
+               FROM pa.rides Limit 50; """)
+        rides = cursor.fetchall()
+    except Exception as e:
+        return jsonify({"error": 'Failed to fetch rides'}), 500
+    finally:
+        cursor.close()
+        release_db_connection(conn)
     return jsonify(rides)
 
 # GET all rides from pa.rides_geojson using a prepared view (00_create_rides_geojson_view)formatted as a proper GeoJSON
@@ -69,10 +91,14 @@ def get_rides2():
 def get_rides3():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("""SELECT * FROM pa.rides_geojson LIMIT 10""")
-    rides = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    try:
+        cursor.execute("""SELECT * FROM pa.rides_geojson LIMIT 50""")
+        rides = cursor.fetchall()
+    except Exception as e:
+        return jsonify({"error": 'Failed to fetch rides'}), 500
+    finally:
+        cursor.close()
+        release_db_connection(conn)
     # Format the all result as a GeoJSON
     rides = format_geojson(rides)
     return jsonify(rides)
@@ -82,15 +108,19 @@ def get_rides3():
 def get_ride(id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM pa.rides_geojson WHERE id = %s", (id,))
-    ride = cursor.fetchall()
-    ride = format_geojson(ride)
-    cursor.close()
-    conn.close()
-
+    try:
+        cursor.execute("SELECT * FROM pa.rides_geojson WHERE id = %s", (id,))
+        ride = cursor.fetchall()
+    except Exception as e:
+        return jsonify({"error": f'Failed to fetch ride {id}'}), 500
+    finally:
+        cursor.close()
+        release_db_connection(conn)
     if not ride:
-        return jsonify({"message": "Ride not found"}), 404
-
+        return jsonify({"message": f"Ride {id} not found"}), 404
+    
+    # Format results as GeoJSON
+    ride = format_geojson(ride)
     return jsonify(ride)
 
 # POST a new ride to sa.rides
@@ -99,7 +129,6 @@ def create_ride():
     body = request.get_json()
     conn = get_db_connection()
     cursor = conn.cursor()
-
     query = """
         INSERT INTO sa.rides (
             pickup_datetime, dropoff_datetime, pickup_latitude, pickup_longitude,
@@ -115,32 +144,49 @@ def create_ride():
         body["passenger_count"], body["rate_code"], body["payment_type"], 
         body["tip_amount"], body["total_amount"]
     )
-
-    cursor.execute(query, values)
-    # This gets the id of the created ride because of the "RETURNING id" in the insert query
-    created_id = cursor.fetchone()["id"]
-    
-    conn.commit()
-    cursor.close()
-    conn.close()
+    try:
+        cursor.execute(query, values)
+        # This gets the id of the created ride because of the "RETURNING id" in the insert query
+        created_id = cursor.fetchone()["id"]
+        conn.commit()
+    except Exception as e:
+        return jsonify({"error": 'Failed to post new ride'}), 500
+    finally:
+        cursor.close()
+        release_db_connection(conn)
     return jsonify({"message": f"New {created_id} ride created"}), 201
 
 # PUT method to update a ride in pa.rides by id
 @app.route('/rides/<id>', methods=['PUT'])
 def update_ride(id):
     body = request.get_json()
+
+    ALLOWED_UPDATE_FIELDS = [
+        "pickup_datetime",
+        "dropoff_datetime",
+        "passenger_count",
+        "rate_code",
+        "payment_type",
+        "tip_amount",
+        "total_amount"
+    ]
+    
     conn = get_db_connection()
     cursor = conn.cursor()
-
     # Build the update query dynamically
-    for key, value in body.items():
-        key = key.replace(';','') # This will sanitize the key to make sure there's no code injection in the query
-        query = "UPDATE pa.rides SET " + key + " = %s WHERE id = %s"
-        cursor.execute(query, (value, id,))
-
-    conn.commit()
-    cursor.close()
-    conn.close()
+    try:
+        for key, value in body.items():
+            # Make sure there's no code injection using the fields
+            if key not in ALLOWED_UPDATE_FIELDS:
+                return jsonify({"error": f"Invalid field: {key}"}), 400
+            query = "UPDATE pa.rides SET " + key + " = %s WHERE id = %s" # TODO: Understand
+            cursor.execute(query, (value, id,))
+        conn.commit()
+    except Exception as e:
+        return jsonify({"error": f'Failed to update ride {id}'}), 500
+    finally:
+        cursor.close()
+        release_db_connection(conn)
 
     return jsonify({"message": f"Ride {id} updated successfully"})
 
@@ -149,10 +195,14 @@ def update_ride(id):
 def delete_ride(id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM sa.rides WHERE id = %s", (id,))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    try:
+        cursor.execute("DELETE FROM pa.rides WHERE id = %s", (id,))
+        conn.commit()
+    except Exception as e:
+        return jsonify({"error": f"Failed to delete ride {id}"}), 500
+    finally:
+        cursor.close()
+        release_db_connection(conn)
     return jsonify({"message": f"Ride {id} was deleted successfully"})
 
 # EXERCISE
